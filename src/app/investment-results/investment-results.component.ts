2ade3f0cd44487d8f7c04c76cbadf8518cba56f3
Alexandre Barreto <plotartemail@gmail.com>
2024-09-30 15:23:03 -0300

    Último commit da aplicação

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,21 +1,17 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
 export class InvestmentResultsComponent {
-  //results = input();
-  @Input() results?: {
-    year: number,
-    interest: number,
-    valueEndOfYear: number,
-    annualInvestment: number,
-    totalInterest: number,
-    totalAmountInvested: number,
-  }[];
-  
+  constructor(private investmentService: InvestmentService){}
+
+  get results(){
+    return this.investmentService.resultData;
+  }
+
 }
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,29 +1,30 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { InvestmentInput } from '../investment-input.model';
+import { Component, signal } from '@angular/core';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
-  standalone: true,
-  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  enteredInicialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
 
-  @Output() calculate = new EventEmitter<InvestmentInput>();
-  enteredInicialInvestment = '0';
-  enteredAnnualInvestment = '0';
-  enteredExpectedReturn = '5';
-  enteredDuration = '10';
+  constructor(private investmentService: InvestmentService){}
 
   onSubmit(){
-    this.calculate.emit({
-      initialInvestment: + this.enteredAnnualInvestment,
-      duration: + this.enteredDuration,
-      expectedReturn: + this.enteredExpectedReturn,
-      annualInvestment: + this.enteredAnnualInvestment
-    })
+    this.investmentService.calculateInvestmentResults({
+      initialInvestment: + this.enteredAnnualInvestment(),
+      duration: + this.enteredDuration(),
+      expectedReturn: + this.enteredExpectedReturn(),
+      annualInvestment: + this.enteredAnnualInvestment()
+    });
+    this.enteredInicialInvestment.set('0')
+    this.enteredAnnualInvestment.set('0')
+    this.enteredExpectedReturn.set('0')
+    this.enteredDuration.set('0')
   }
 
 }
