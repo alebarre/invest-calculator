465eaa7dc80b1760943bc60bea0860b2872b2b17
Alexandre Barreto <plotartemail@gmail.com>
2024-09-30 05:35:11 -0300

    marcacao html e css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
 
 @Component({
   selector: 'app-root',
+  imports: [HeaderComponent, UserInputComponent],
   standalone: true,
   templateUrl: './app.component.html',
 })
